746c697a760c2a2b2218da6be6cd018b1bb72cd8
edward487 <drawdej963@gmail.com>
2017-10-29 07:31:47 -0700

    added css and navbar

---
+++ navbar.php
@@ -0,0 +1,31 @@
+<link href="vendors/bootstrap/css/bootstrap3/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="fonts/Font-Awesome/css/font-awesome.css">
+<nav class="navbar bg-black no-pad" style="margin-top: 60px;">
+    <div class="container-fluid col-md-offset-1">
+        <ul class="nav navbar-nav">
+            <li class="active col-md-4"><a href="#" class="f-18">Home</a></li>
+            <li class="col-md-4"><a href="#" class="f-18">About</a></li>
+            <li class="col-md-4"><a href="#" class="f-18">Contacts</a></li>
+        </ul>
+        <div class="container">
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 col-md-offset-3 col-sm-offset-1 col-xs-offset-1"  id="search-gone" style="position:relative; top:20px;">
+                <form action="" class="search-form icon-pos">
+                    <div class="form-group has-feedback">
+                        <label for="search" class="sr-only">Search</label>
+                        <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                        <i class="fa fa-search icon-x3"></i>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-1 col-md-1 col-sm-2 col-xs-2">
+                <button href="#" class="btn bg-black"><i class="fa fa-user icon-x3 icon-pos" style="font-size: 2.5em;"></i>Name</button>
+            </div>
+        </div>
+
+    </div>
+</nav>
+    <a class="navbar-link" href="index.php"><img src="img/logo.png" style="width:120px; height: 120px; position:relative; margin-top:-110px; margin-left: 600px;"></a> 
+<script type="text/javascript" src="vendors/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="vendors/bootstrap/js/popper.min.js"></script>
+<script type="text/javascript" src="vendors/bootstrap/js/bootstrap.min.js"></script>
