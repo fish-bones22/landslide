fca15ae7ae69e376da4c3c9a13d16bdab03b92c0
edward487 <drawdej963@gmail.com>
2017-10-30 00:01:02 -0700

    added temporary top products to carousel

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<title>Landslide</title>
-	<link href="vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="vendors/bootstrap3/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
-
+<body class="bg-gray2">
+	<?php include'navbar.php'; ?>
 	<?php 
 
 		require_once 'php/objects/objProduct.php';
@@ -17,39 +17,82 @@
 		$_SESSION["isdev"] = true;
 
 	?>
-
 	<div class="container">
 
-		<h1>Landslide</h1>
-		<div class="display-3">Online Store</div>
-
-		<div class="form-group">
-			<form action="product-drawer.php" method="get">
-				<input type="text" name="search" />
-				<input type="submit" class="btn btn-primary" value="Search">
-			</form>
-		</div>
-
+		<h1 class="f-40"><strong>Landslide</strong></h1><br>
+		
+		<p class="f-24">Top Products</p>
+		<!--Carousel-->
+		<div id="thumbnail-preview-indicators" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#thumbnail-preview-indicators" data-slide-to="0" class="active">
+						<div class="thumbnail">
+							<img class="img-responsive" src='img/job11.jpg' />
+						</div>
+					</li>
+					<li data-target="#thumbnail-preview-indicators" data-slide-to="1">
+						<div class="thumbnail">
+							<img class="img-responsive" src='img/job11.jpg' />
+						</div>
+					</li>
+					<li data-target="#thumbnail-preview-indicators" data-slide-to="2">
+						<div class="thumbnail">
+							<img class="img-responsive" src='img/job11.jpg' />
+						</div>
+					</li>
+				</ol>
+				<div class="carousel-inner">
+				<div class="item slides active">
+				<div class="slide-1"></div>
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>THESIS DEFENSE IS COMING</h1>
+							<p>Wala pa ring chapter 4 at 5</p>
+						</div>
+					</div>
+				</div>
+				<div class="item slides">
+					<div class="slide-2"></div>
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>THESIS DEFENSE IS COMING</h1>
+							<p>Wala pa ring chapter 4 at 5</p>
+						</div>
+					</div>
+				</div>
+				<div class="item slides">
+					<div class="slide-3"></div>
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>THESIS DEFENSE IS COMING</h1>
+							<p>Wala pa ring chapter 4 at 5</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		<a class="left carousel-control" href="#thumbnail-preview-indicators" role="button" data-slide="prev"><i class="fa fa-chevron-left chevron-pos" style="font-size:2.5em;"></i></a>
+		<a class="right carousel-control" href="#thumbnail-preview-indicators" role="button" data-slide="next"><i class="fa fa-chevron-right chevron-pos" style="font-size:2.5em;"></i></a>
+	</div> 
 		<div class="row">
 
 			<!-- Top Products -->
 			<div class="row">
 				<div class="display-4 col-md-12">Top Products</div>
 				<?php
-					
-					$top_prod = Product::getTopProducts(5);
 
-					if (!$top_prod) {
+				$top_prod = Product::getTopProducts(5);
 
-						echo "<div>No Products in this category</div>";
+				if (!$top_prod) {
 
-					} else {
-						
-						foreach ($top_prod as $product) {
+					echo "<div>No Products in this category</div>";
 
-							if ($product->approval != 1 && !$_SESSION["isdev"]) continue;
+				} else {
 
-							echo "
+					foreach ($top_prod as $product) {
+
+						if ($product->approval != 1 && !$_SESSION["isdev"]) continue;
+
+						echo "
                                         <a href='product.php?id=$product->id' class='row product-box'>
                                         <div class='product_thumbnail_container'>
                                             <img class='product_thumbnail' src='".$product->icon_location."' />
@@ -64,31 +107,31 @@
                                         </div>
                                     </a>
 							";
-						}
 					}
+				}
 
 				?>
 			</div>
 			<!-- End of Top -->
 
 			<!-- Newest -->
-                <div class="row">
-                    <div class="display-4 col-md-12">New Products</div>
-                    <?php
+			<div class="row">
+				<div class="display-4 col-md-12">New Products</div>
+				<?php
 
-                        $new_prod = Product::getNewProducts(5);
+				$new_prod = Product::getNewProducts(5);
 
-                        if (!$new_prod) {
+				if (!$new_prod) {
 
-                            echo "<div>No Products in this category</div>";
+					echo "<div>No Products in this category</div>";
 
-                        } else {
+				} else {
 
-                            foreach ($new_prod as $product) {
+					foreach ($new_prod as $product) {
 
-                                if ($product->approval != 1 && !$_SESSION["isdev"]) continue;
+						if ($product->approval != 1 && !$_SESSION["isdev"]) continue;
 
-                                echo "
+						echo "
                                         <a href='product.php?id=$product->id' class='row product-box'>
                                             <div class='product_thumbnail_container'>
                                                 <img class='product_thumbnail' src='".$product->icon_location."' />
@@ -103,10 +146,10 @@
                                             </div>
                                         </a>
                                 ";
-                            }
-                        }
-                    ?>
-                </div>
+					}
+				}
+				?>
+			</div>
 			<!-- End of Newest -->
 
 		</div>
--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
-<link href="vendors/bootstrap3/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="fonts/Font-Awesome/css/font-awesome.css">
+<link href="vendors/bootstrap3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
 <nav class="navbar bg-black" style="margin-top: 60px;">
     <div class="container-fluid">
         <ul class="nav navbar-nav" style="margin-left:120px;">
@@ -8,18 +8,18 @@
             <li class="col-md-4"><a href="#" class="f-18">About</a></li>
             <li class="col-md-4"><a href="#" class="f-18">Contacts</a></li>
         </ul>
-        <div class="col-sm-3 col-md-3 col-md-offset-3" style="margin-top:5px;">
+        <div class="col-sm-3 col-md-3 col-md-offset-3">
             <form class="navbar-form" role="search">
                 <div class="input-group">
                     <div class="input-group-btn">
                         <button class="btn bg-gray btn-rad" type="submit"><i class="fa fa-search" style="font-size: 0.9em;"></i></button>
                     </div>
-                    <input type="text" placeholder="Search" style="color:#000000;">
+                    <input type="text" class="form-control" placeholder="Search">
                 </div>
             </form>
         </div>
         <ul class="nav navbar-nav">
-            <li class="dropdown"><a class="dropdown-toggle bg-black" data-toggle="dropdown" href="#"><i class="fa fa-user icon-x3" style="font-size: 1.5em;"></i>Name</a>
+            <li class="dropdown"><a class="dropdown-toggle bg-black" data-toggle="dropdown" href="#"><i class="fa fa-user icon-x3" style="font-size: 1.5em;"></i>--Name--</a>
                 <ul class="dropdown-menu">
                     <li><a href="#" class="f-18">Settings</a></li>
                     <li class="divider"></li>
@@ -32,5 +32,4 @@
 </nav>
     <a class="navbar-link" href="index.php"><img src="img/logo.png" style="width:140px; height: 140px; position:relative; margin-top:-115px; margin-left: 580px;"></a> 
 <script type="text/javascript" src="vendors/jquery/jquery.min.js"></script>
-<script type="text/javascript" src="vendors/bootstrap/js/popper.min.js"></script>
-<script type="text/javascript" src="vendors/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="vendors/bootstrap3/js/bootstrap.min.js"></script>
